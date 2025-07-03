e92f4d01a7b663597a45fa5c97718119fe18d6b3
Abrorbek <abrorbekikromiddinov@gmail.com>
2025-07-03 07:13:55 -0700

    update

--- src/pages/myAccount/MyAccount.jsx
+++ src/pages/myAccount/MyAccount.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./MyAccount.css";
 
 function MyAccount() {
@@ -19,22 +19,66 @@ function MyAccount() {
                 <div className="menus">
                   <h3>Manage My Account</h3>
                   <span>
-                    <Link>My Profile</Link>
-                    <Link>Address Book</Link>
-                    <Link>My Payment Options</Link>
+                    <NavLink>My Profile</NavLink>
+                    <NavLink>Address Book</NavLink>
+                    <NavLink>My Payment Options</NavLink>
                   </span>
                 </div>
 
                 <div className="menus">
                   <h3>Manage My Account</h3>
                   <span>
-                    <Link>My Returns</Link>
-                    <Link>My Cancellations</Link>
+                    <NavLink>My Returns</NavLink>
+                    <NavLink>My Cancellations</NavLink>
                   </span>
                 </div>
 
                 <div className="menus">
-                    <h3>My WishList</h3>
+                  <h3>My WishList</h3>
+                </div>
+              </div>
+
+              <div className="accountRight">
+                <div className="profils">
+                  <div className="topProfils">
+                    <h3>Edit Your Profile</h3>
+                    <div className="titleInputs">
+                      <span>
+                        <label htmlFor="">First Name</label>
+                        <input type="text" placeholder="Md" />
+                      </span>
+                      <span>
+                        <label htmlFor="">Last Name </label>
+                        <input type="text" placeholder="Rimel" />
+                      </span>
+                      <span>
+                        <label htmlFor="">Email</label>
+                        <input type="text" placeholder="rimel1111@gmail.com" />
+                      </span>
+                      <span>
+                        <label htmlFor="">Address</label>
+                        <input
+                          type="text"
+                          placeholder="Kingston, 5236, United State"
+                        />
+                      </span>
+                    </div>
+
+                    <div className="bottomProfiles">
+                      <h4>Password Changes</h4>
+
+                      <div className="bottomInputs">
+                        <input type="text" placeholder="Current Passwod" />
+                        <input type="text" placeholder="New Passwod" />
+                        <input type="text" placeholder="Confirm New Passwod" />
+                      </div>
+                    </div>
+
+                    <div className="profilBtn">
+                        <button className="profilBtn1">Cancel</button>
+                        <button className="profilBtn2">Save Changes</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
